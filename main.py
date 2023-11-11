324e00c25e67b2e9410358eb7a108962a737bb8f
KagruaChen <2684607398@qq.com>
2023-11-11 17:12:17 +0800

    Refactoring

--- main.py
+++ main.py
@@ -7,149 +7,167 @@ import asyncio
 import requests
 import re
 from bs4 import BeautifulSoup
+from threading import Thread
 from concurrent.futures import ThreadPoolExecutor
+from typing import Optional
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
+WIDTH, HEIGHT = 600, 400    # 窗口大小
+PADDING = 4                # 右下角窗口边距
+OFFSET_X, OFFSET_Y = 500, 300   # 窗口偏移量
+
+class DownloadUI(tk.Frame):    # 下载UI
+    def __init__(self, master: tk.Tk=None) -> None:
         super().__init__(master)
-        self.place()
-        self.address = os.getcwd()       # 当前路径
-        self.time=0     # 记录下载次数
+        self.place()    # 采用place布局
+        self.download_core = DownloadCore()    # 下载内核
+        if not os.path.exists("Downloads"):
+            os.mkdir("Downloads")
+        self.path = os.getcwd() + "\Downloads"      # 当前路径
+        self.ui_init()
 
-        self.creatUI()
-
-    def creatUI(self):
+    def ui_init(self) -> None:
         # 创建背景图片
-        self.photo = tk.PhotoImage(file=f"bg.png").subsample(2)
-        self.bp = tk.Canvas(width=600,height=400)
-        self.bp.place(x=0, y=0)
-        self.bp.create_image(0, 0, image=self.photo, anchor='nw')
+        self.bp = tk.PhotoImage(file=f"bg.png").subsample(2)
+        self.bp_canvas = tk.Canvas(width=WIDTH, height=HEIGHT)
+        self.bp_canvas.place(x=0, y=0)
+        self.bp_canvas.create_image(0, 0, image=self.bp, anchor='nw')
         # 创建标签
-        self.label1 = tk.Label(self.master, text="①专栏地址:", font=("黑体", 12))
-        self.label1.place(relx=0.08, rely=0.05)
-        self.label2 = tk.Label(self.master, text="暂无下载内容", justify="center", width=40, height=1, fg="black")
-        self.label2.place(relx=0.2, rely=0.83)
-        self.label3 = tk.Label(self.master, width=40, justify="center", height=1, fg="black", text="当前路径为："+self.address, anchor="w")
-        self.label3.place(relx=0.2, rely=0.88)
-        self.label4 = tk.Label(self.master, text="②空间地址:", font=("黑体", 12))
-        self.label4.place(relx=0.08, rely=0.15)
-        self.label5 = tk.Label(self.master, text=" 空间页数:", font=("黑体", 12))
-        self.label5.place(relx=0.08, rely=0.25)
+        LABEL_FONT_SIZE = 13
+        LABEL_RELX = 0.1
+        LABEL_WIDTH = 68
+        self.column_label = tk.Label(text="①专栏地址:", font=("黑体", LABEL_FONT_SIZE))
+        self.column_label.place(relx=LABEL_RELX, rely=0.05)
+        self.info_label = tk.Label(text="暂无下载内容", justify="center", width=LABEL_WIDTH, height=1, fg="black")
+        self.info_label.place(relx=LABEL_RELX, rely=0.83)
+        self.path_label = tk.Label(text="当前路径为：" + self.path, width=LABEL_WIDTH, justify="center", height=1, fg="black", anchor="w")
+        self.path_label.place(relx=LABEL_RELX, rely=0.88)
+        self.space_label = tk.Label(text="②空间地址:", font=("黑体", LABEL_FONT_SIZE))
+        self.space_label.place(relx=LABEL_RELX, rely=0.15)
+        self.page_label = tk.Label(text=" 空间页数:", font=("黑体", LABEL_FONT_SIZE))
+        self.page_label.place(relx=LABEL_RELX, rely=0.25)
         # 创建输入框
-        self.url1 = tk.StringVar()
-        self.url2 = tk.StringVar()
-        self.url3 = tk.StringVar()
-        self.url3.set("1-3")
-        self.entry1 = tk.Entry(self.master, width=30, textvariable=self.url1)
-        self.entry1.place(relx=0.25, rely=0.05)
-        self.entry2 = tk.Entry(self.master, width=30, textvariable=self.url2)
-        self.entry2.place(relx=0.25, rely=0.15)
-        self.entry3 = tk.Entry(self.master, width=6, textvariable=self.url3)
-        self.entry3.place(relx=0.25, rely=0.25)
+        ENTRY_WIDTH = 38
+        ENTRY_RELX = 0.28
+        self.column_value = tk.StringVar()
+        self.space_value = tk.StringVar()
+        self.page_value = tk.StringVar()
+        self.page_value.set("1-3")
+        self.column_entry = tk.Entry(width=ENTRY_WIDTH, textvariable=self.column_value)
+        self.column_entry.place(relx=ENTRY_RELX, rely=0.05)
+        self.space_entry = tk.Entry(width=ENTRY_WIDTH, textvariable=self.space_value)
+        self.space_entry.place(relx=ENTRY_RELX, rely=0.15)
+        self.page_entry = tk.Entry(width=7, textvariable=self.page_value)
+        self.page_entry.place(relx=ENTRY_RELX, rely=0.25)
         # 创建按钮
-        self.button1 = tk.Button(self.master, text="提交", font=("黑体", 15), height=1, command=self.sumit)
-        self.button1.place(relx=0.75,rely=0.05)
-        self.button2 = tk.Button(self.master, text="更\n改\n路\n径", font=("黑体", 15), height=4, command=self.addressChange)
-        self.button2.place(relx=0.85,rely=0.04)
-        self.button3 = tk.Button(self.master, text="提交", font=("黑体", 15), height=1, command=self.uiddownload)
-        self.button3.place(relx=0.75, rely=0.15)
+        self.column_btn = tk.Button(text="提交", font=("黑体", 10), height=1,
+                                    command=lambda : Thread(target=self.column_download).start())
+        self.column_btn.place(relx=0.75,rely=0.05)
+        self.change_path_btn = tk.Button(text="更\n改\n路\n径", font=("黑体", 12), height=4, command=self.change_path)
+        self.change_path_btn.place(relx=0.85, rely=0.04)
+        self.space_btn = tk.Button(text="提交", font=("黑体", 10), height=1,
+                                   command=lambda : Thread(target=self.space_download).start())
+        self.space_btn.place(relx=0.75, rely=0.15)
 
-    def addressChange(self):
+    def change_path(self) -> None:  # 更改下载路径
         address = tk.filedialog.askdirectory()
         if address != '':
-            self.address = address  # 获得选择的路径
-            self.label3.configure(text=f"当前路径为：{self.address}")
+            self.address = address
+            self.path_label.configure(text=f"当前路径为：{self.address}")
 
-    def sumit(self,url='', address=''):
+    def column_download(self) -> None:  # 一个专栏图片下载
+        self.info_label.configure(text="正在下载中......")
+        times = self.download_core.column_download(self.column_value.get(), self.path)
+        if (times is None):
+            self.info_label.configure(text="地址输入错误！")
+        else:
+            self.info_label.configure(text=f"{times}次下载已完成")
+
+    def space_download(self) -> None:   # 按页数下载空间的专栏图片
+        self.info_label.configure(text="正在下载中......")
+        times = self.download_core.space_download(self.space_value.get(), self.path, self.page_value.get())
+        if (times is None):
+            self.info_label.configure(text="地址或页数输入错误！")
+        else:
+            self.info_label.configure(text=f"{times}次下载已完成")
+
+class DownloadCore():   # 下载内核
+    def __init__(self) -> None:
+        self.times = 0
+        self.headers = {
+            'referer': 'https://space.bilibili.com/9277299/article',    # 防盗链，随便找一个专栏地址
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36'
+        }
+        self.obj = re.compile('"id":(?P<id>\d+),"category.*?],"title":"(?P<title>.*?)","summary"')
+
+    def column_download(self, url: str, path: str) -> Optional[int]:
         try:
-            self.label2.configure(text="正在下载中......")
             # 页面解析获得图片地址
-            if url == '':
-                url = self.entry1.get()
-            headers = {
-                'referer': 'https://space.bilibili.com/9277299/article',
-                'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36'
-            }
-            resp = requests.get(url, headers=headers)
-            page = BeautifulSoup(resp.text, "html.parser")
-            imgs = page.find_all("img", class_=None)
-            # 异步协程下载图片
-            asyncio.run(self.main(imgs,address))
+            resp = requests.get(url, headers=self.headers)
+            imgs = BeautifulSoup(resp.text, "html.parser").find_all("img", class_=None)
+            # 异步下载图片
+            asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy()) # 解决RuntimeError: Event loop is closed
+            asyncio.run(self.async_main(imgs, path))
 
-            self.time += 1
-            self.label2.configure(text=f"{self.time}次下载已完成")
-        except:
-            self.label2.configure(text="地址输入错误！")
+            self.times += 1
+            return self.times
+        except Exception as e:
+            print(e.with_traceback())
+            return None
 
-    async def main(self, imgs, address):
+    async def async_main(self, imgs, path) -> None:
         tasks = []
         for img in imgs:
             src = img.get("data-src")
-            name = src.split("/")[-1]
+            name = path + "\\" + src.split("/")[-1]
             src = "https:" + src
-            tasks.append(asyncio.create_task(self.download(src, name, address)))
-        await asyncio.wait(tasks)
+            tasks.append(asyncio.create_task(self.picture_download(src, name)))
+        await asyncio.gather(*tasks)
 
-    async def download(self, src, name, address):
+    async def picture_download(self, src: str, path: str) -> None:
         async with aiohttp.ClientSession() as session:
             async with session.get(src) as html:
-                content = await html.read()     # 读取内容
-                if address == '':
-                    async with aiofiles.open(f"{self.address}/{name}", mode="wb") as f:
-                        await f.write(content)
-                else:
-                    async with aiofiles.open(f"{address}/{name}", mode="wb") as f:
-                        await f.write(content)
+                content = await html.read()  # 读取内容
+                async with aiofiles.open(path, mode="wb") as f:
+                    await f.write(content)
 
-    def uiddownload(self):
-        # 判断地址是否输入正确
-        first_url = self.entry2.get()
+    def space_download(self, url: str, path: str, page: str) -> Optional[int]:
+        # 判断地址和页数是否输入正确
         try:
-            response = requests.get(first_url)
-        except:
-            self.label2.configure(text="地址输入错误！")
-            return
-        # url处理
-        UID = first_url.split('/')[-1]
-        # 预处理
-        obj = re.compile('"id":(?P<id>\d+),"category.*?],"title":"(?P<title>.*?)","summary"')
-        headers = {
-            'referer': 'https://space.bilibili.com/9277299/article',
-            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36'
-        }
+            response = requests.get(url)
+            page = page.split('-')
+            start, end = int(page[0]), int(page[1])
+        except Exception as e:
+            print(e.with_traceback())
+            return None
+        UID = url.split('/')[-1]  # 获得空间的UID
 
-        flag = True  # 判断循环
-        i = 0  # 页数
-        page = self.entry3.get().split('-')
-        start = int(page[0])
-        end = int(page[1])
+        flag = True  # 判断是否越界
+        index = start if start >= 0 else 0  # 页数
         while (flag):
-            i += 1
-            if i<start:
-                continue
-            if i>end:
+            if index > end:
                 break
             flag = False
-            second_url = 'https://api.bilibili.com/x/space/article?mid=' + UID + '&pn=' + str(
-                i) + '&ps=12&sort=publish_time&jsonp=jsonp&callback=__jp1'
-            resp = requests.get(second_url, headers=headers)
+            url = 'https://api.bilibili.com/x/space/article?mid=' + UID + '&pn=' + str(
+                index) + '&ps=12&sort=publish_time&jsonp=jsonp&callback=__jp1'
+            resp = requests.get(url, headers=self.headers)
             resp.encoding = 'utf-8'
-            ids = obj.finditer(resp.text)
-            with ThreadPoolExecutor(30) as t:
+            ids = self.obj.finditer(resp.text)  # 获得的本页的专栏id和标题
+            with ThreadPoolExecutor(20) as t:
                 for id in ids:
-                    address = self.address + '/' + id.group("title").replace('/','|').replace(':','|')
-                    os.mkdir(address)
-                    url = "https://www.bilibili.com/read/cv"+id.group("id")+"?spm_id_from=333.999.0.0"
-                    t.submit(self.sumit(url=url,address=address))
+                    dir_path = path + '\\' + id.group("title").replace('/', '|').replace(':', '|')
+                    if not os.path.exists(dir_path):
+                        os.mkdir(dir_path)
+                    url = "https://www.bilibili.com/read/cv" + id.group("id") + "?spm_id_from=333.999.0.0"
+                    t.submit(self.column_download(url, dir_path))
                     flag = True
-
-
+            index += 1
+        return self.times
 
 if __name__ == "__main__":
-    bili = tk.Tk()
-    bili.geometry("607x407+400+200")
-    bili.resizable(width=False, height=False)   # 设置无法更改比例
-    bili.title("B站专栏图片下载")
-    bili.attributes("-alpha", 0.9)      # 设置透明度
-    Application(master = bili)
-    bili.mainloop()
+    application = tk.Tk()
+    application.geometry(f"{WIDTH + PADDING}x{HEIGHT + PADDING}+{OFFSET_X}+{OFFSET_Y}")
+    application.resizable(width=False, height=False)  # 设置无法更改比例
+    application.title("B站专栏图片下载")
+    application.attributes("-alpha", 0.9)  # 设置透明度
+    DownloadUI(application)
+    application.mainloop()
